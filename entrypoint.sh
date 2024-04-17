ded72b3dee1edd51c086e4965db80188e67e0926
Neothe2 <neo.j.jabin@gmail.com>
2024-04-18 01:38:50 +0530

    modified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Load data from groups.json
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py loaddata groups.json
 
 # Run the original command (e.g., Gunicorn)
