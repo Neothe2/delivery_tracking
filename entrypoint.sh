3f6857ff4f5562f082c8f8633c664604b879ef9b
Neothe2 <neo.j.jabin@gmail.com>
2024-04-18 01:55:26 +0530

    no input createsuperuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata groups.json
+python3 manage.py createsuperuser --noinput
 
 # Run the original command (e.g., Gunicorn)
 exec "$@"
