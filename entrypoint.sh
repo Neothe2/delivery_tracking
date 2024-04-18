5b8d5fefd5d533ce2732b4a580490987bb027b43
Neothe2 <neo.j.jabin@gmail.com>
2024-04-18 13:20:25 +0530

    removed superuser password from environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata groups.json
-python3 manage.py createsuperuser --noinput
 
 # Run the original command (e.g., Gunicorn)
 exec "$@"
